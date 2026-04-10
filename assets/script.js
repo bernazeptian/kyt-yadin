9bff5b15a0be86953eeed16ec3a8dbf994be36d2
Bernaz <bernazseptian@gmail.com>
2026-04-10 14:11:42 +0700

    new

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ const msalConfig = {
   auth: {
     clientId: "5d4a75cc-0336-4450-80c3-8974357fb0d5",        // from Azure Portal
     authority: "https://login.microsoftonline.com/85f795ce-af26-4489-a7a3-35d9420bd137",  // from Azure Portal
-    redirectUri: "http://localhost/kyt-yadin/"  // e.g. http://localhost/auth/login.php
+    redirectUri: "http://kyt.yadin.com/"  // e.g. http://localhost/auth/login.php
   }
 };
 
