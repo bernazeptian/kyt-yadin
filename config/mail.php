14c5d5beb23505f0cbc489f3386f20c83befa76a
Bernaz <bernazseptian@gmail.com>
2026-04-13 10:38:00 +0700

    change password

--- config/mail.php
+++ config/mail.php
@@ -9,7 +9,7 @@ define('MAIL_HOST', 'smtp-relay.brevo.com');
 define('MAIL_PORT', 587);        // ← 587 for TLS, 465 for SSL
 define('MAIL_ENCRYPTION', 'tls');      // ← tls or ssl
 define('MAIL_USERNAME', 'a7eb4f001@smtp-brevo.com');
-define('MAIL_PASSWORD', 'xsmtpsib-4ce0524faa9302764eefa1ff679e3ef0fcd584851de76b16918541d4f79abc37-l49k4N9z5DXoNHAQ');
+define('MAIL_PASSWORD', 'xsmtpsib-4ce0524faa9302764eefa1ff679e3ef0fcd584851de76b16918541d4f79abc37-eJaj4GZSsD1esVSb');
 define('MAIL_FROM_NAME', 'KYT Yadin System');
 define('MAIL_FROM_EMAIL', 'noreply@yanmar.co.id');
 define('APP_URL', 'http://kyt.yadin.com');
